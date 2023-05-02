b3d27b5ef291bf3c5c60778011fb5b83a3929cfb
AAbir <110724190+aabbirbapi@users.noreply.github.com>
2023-05-02 22:47:08 +0530

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+
+  subscription_id = "fe6c1298-1263-477b-b9b5-3f2295ef0c6c"
+  tenant_id       = "eeba282e-fc35-46ee-a40a-d51c8e4f149f"
+  client_id       = "bc3a609f-4d5f-4930-98ed-d8c3b634045d"
+  client_secret   = "CIX8Q~xlzHUCzFzSk4DHmGeKvp_Q9q-pft4J7aBX"
+  features {}
+}
+
+  
