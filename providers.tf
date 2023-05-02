e1ba562a0d440a23fb256d66b7da0cfe5fdb12f0
AAbir <110724190+aabbirbapi@users.noreply.github.com>
2023-05-02 22:47:54 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
 provider "azurerm" {
 
   subscription_id = "fe6c1"
-  tenant_id       = "eeba282e-fc35-46ee-a40a-d51c8e4f149f"
+  tenant_id       = "eeba"
   client_id       = "bc3a609f-4d5f-4930-98ed-d8c3b634045d"
   client_secret   = "CIX8Q~xlzHUCzFzSk4DHmGeKvp_Q9q-pft4J7aBX"
   features {}
